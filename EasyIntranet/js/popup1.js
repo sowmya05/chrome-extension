779debfa2f683344362aaaae8d70bec47888048f
sowmya05 <sowmyak.05@gmail.com>
2015-04-18 12:41:18 +0530

    chrome extension

---
+++ EasyIntranet/js/popup1.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+        $('#horizontalTab').easyResponsiveTabs({
+            type: 'default',            
+            width: 'auto', 
+            fit: true,   
+            closed: 'accordion', 
+            activate: function(event) { 
+                var $tab = $(this);
+                var $info = $('#tabInfo');
+                var $name = $('span', $info);
+
+                $name.text($tab.text());
+
+                $info.show();
+            }
+        });
+
+        $('#verticalTab').easyResponsiveTabs({
+            type: 'vertical',
+            width: 'auto',
+            fit: true
+        });
+    });
